532ee2f2f889c047298a1b3860f7dfa863fa93f9
Darkn3ssF4lls <trekfan2004@gmail.com>
2019-02-10 14:26:43 +0900

    Deleted mymode, added chapter five homework, changed final project source

--- mymode.R
+++
@@ -1,7 +0,0 @@
-mode <- function(input1){
-  uniqueValues <- unique(input1)
-  uniqueCounts <- tabulate(match(input1,uniqueValues))
-  output <- uniqueValues[which.max(uniqueCounts)]
-  return (output)
-}
-tinyData <- c(1,2,1,2,3,3,3,4,5,4,5)
