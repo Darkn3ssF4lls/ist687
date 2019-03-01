c6cda619408d0c10c35fbf27bcd1ef4dabde9faa
Darkn3ss F4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-03-01 09:47:06 +0900

    almost time to turn in

--- HW7/LewisDB_HW7.R
+++ HW7/LewisDB_HW7.R
@@ -13,9 +13,6 @@
 ############################LOCAL FUNCTIONS##################################
 #############################################################################
 #
-dedot <- function(inputVector){
-  inputVector <- gsub("\\.","",csv_import)
-}
 EnsurePackage<-function(x){
    x<-as.character(x)
    if (!require(x,character.only=TRUE)){
@@ -46,8 +43,7 @@ EnsurePackage("zipcode")
 ###[sets working directory to the script folder]#
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
-#csv_import<-read.csv("MedianZIP-3.csv", stringsAsFactors = FALSE)
-csv_import<-read.csv("Median-Cleaned.csv", stringsAsFactors = FALSE)
+csv_import<-read.csv("MedianZIP-3.csv", stringsAsFactors = FALSE)
 #
 #############################################################################
 #############################Problems Solved#################################
@@ -62,32 +58,32 @@ csv_import<-read.csv("Median-Cleaned.csv", stringsAsFactors = FALSE)
 ##4) Merge the zip code information from the two data frames (merge into one dataframe)
 ##5) Remove Hawaii and Alaska (just focus on the lower 48 states)
 #############################################################################
-
+#
+#remove the non numbers 
+csv_import<-csv_import[-c(29980,29645,26201,26134,26133,26132,7056)]
 #turn the col names to lowercase#
 colnames(csv_import)<-tolower(colnames(csv_import))
-
+#
 #clean up the data frame#
-
 csv_import<-data.frame(sapply(csv_import, Numberize))
-
+#
 #fix the zip column length#
 csv_import$zip<-str_pad(csv_import$zip, 5, "left","0")
-
+#
 #generate zipcode data frame#
 data(zipcode)
-
+#
 #perform a merge based on zipcode col#
 zipcode_joincsv<-merge(x=zipcode, y=csv_import, by="zip")
-
+#
 #sort by abv
 colnames(zipcode_joincsv)[3]<-"abv"
 zipcode_joincsv$abv<-sort(zipcode_joincsv$abv)
-
+#
 #Restrict to Lower 48 States (excluding all US territories)#
 zipcode_joincsv<-data.frame(sqldf("SELECT *
-                                  FROM zipcode_joincsv
-                                  WHERE abv NOT IN ('AK', 'HI', 'DC')", row.names=TRUE))
-
+                                 FROM zipcode_joincsv
+                                WHERE abv NOT IN ('AK', 'HI', 'DC')", row.names=TRUE))
 #
 #Step 2: Show the income & population per state
 ##1) Create a simpler dataframe, with just the average median income and the the population for each state.
@@ -103,7 +99,7 @@ abv<-abv[-10]
 #places state and abv into a data frame#
 step2_df<-data.frame(states, abv)
 #sums the population by states and puts them in the data frame#
-population<-sqldf("SELECT AVG(pop) AS 'avgpop'
+population<-sqldf("SELECT sum(pop) AS 'pop'
                   FROM zipcode_joincsv 
                   GROUP BY abv")
 #generates the medium income by state and stores it into a vector#
@@ -124,7 +120,7 @@ map.incomeColor
 #
 us<-map_data("state")
 map.popColor<-ggplot(step2_df,aes(map_id=states))
-map.popColor<-map.popColor+ geom_map(map=us, aes(fill=avgpop))
+map.popColor<-map.popColor+ geom_map(map=us, aes(fill=pop))
 map.popColor<-map.popColor+expand_limits(x=us$long, y=us$lat)
 map.popColor<-map.popColor+coord_map()+ggtitle("States by Population")
 map.popColor
@@ -136,31 +132,33 @@ map.popColor
 step3_df<-data.frame(as.character(tolower(state.name)),as.character(state.abb))
 colnames(step3_df)<-c("state", "abv")
 step3_df<-data.frame(sqldf("SELECT *
-                            FROM step3_df
-                            WHERE abv NOT IN ('AK', 'HI', 'DC')", row.names=TRUE))
+                         FROM step3_df
+                         WHERE abv NOT IN ('AK', 'HI', 'DC')", row.names=TRUE))
 step3_df<-dplyr::left_join(zipcode_joincsv, step3_df, by="abv")
 step3_df$state<-sapply(step3_df$state,as.character)
 step3_df<-step3_df[,c(9,3,2,1,4,5,6,7,8)]
 rownames(step3_df)<-NULL
-
+#
 #show each zipcode on the map where color of dot is based on mean income#
 us<-map_data("state")
 map.zipIncome<-ggplot(step3_df, aes(map_id=state))
 map.zipIncome<-map.zipIncome+geom_map(map=us, color="white")
 map.zipIncome<-map.zipIncome+expand_limits(x=us$long, y=us$lat)
 map.zipIncome<-map.zipIncome+coord_map() + ggtitle("Map of US Zipcodes Colored by Income")
-map.zipIncome <- map.zipIncome + geom_point(data=step3_df, aes(x = longitude, y = latitude), alpha=.5, size=2, color="yellow")
+map.zipIncome <- map.zipIncome + geom_point(data=step3_df, aes(x = longitude, y = latitude), alpha=.5, size=2, color="blue")
 map.zipIncome
 #
 #Step 4: Show Zip Code Density
-##1) Now generate a different map, one where we can easily see where there are lots of zip codes, and where there are few (using the stat_density2dâ function).
+##1) Now generate a different map, one where we can easily see where there are lots of zip codes, and where there are few (using the stat_density2 function).
 #
+step4_df<-step3_df
 us<-map_data("state")
-map.zipDensity<-ggplot(step3_df, aes(map_id=state))
+map.zipDensity<-ggplot(step4_df, aes(map_id=state))
 map.zipDensity<-map.zipDensity+geom_map(map=us, color="white")
 map.zipDensity<-map.zipDensity+expand_limits(x=us$long, y=us$lat)
 map.zipDensity<-map.zipDensity+coord_map() + ggtitle("Map of US Zipcodes Colored by Zipcode Density")
-
+map.zipDensity<-map.zipDensity+stat_density2d( data=step4_df, aes(x=longitude, y=latitude, color=median)) 
+map.zipDensity
 #
 #Step 5: Zoom in to the region around NYC
 ##1) Repeat steps 3 & 4, but have the image / map be of the northeast U.S. (centered around New York).
