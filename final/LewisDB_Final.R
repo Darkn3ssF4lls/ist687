93b643f8fb30929405c5ad6880eb69d20eb4c373
Darkn3ssF4lls <trekfan2004@gmail.com>
2019-04-01 10:13:26 +0900

    Halfway through step 4

--- final/LewisDB_Final.R
+++ final/LewisDB_Final.R
@@ -40,13 +40,18 @@ cleanCSV$vic_race<-tolower(cleanCSV$vic_race)
 
 cleanCSV$occur_date<-removeTime(cleanCSV$occur_date)
 
-cleanCSV<-cleanCSV[,-7]
+cleanCSV<-cleanCSV[,-6:-7]
 colnames(cleanCSV)
 
 cleanCSV<-cleanCSV[complete.cases(cleanCSV),]
 
 ####################Creating Descriptive Statistics###########
 
+knitr::kable(summary(cleanCSV[2:6]), "latex", booktabs=T)
+knitr::kable(summary(cleanCSV[7:11]), "latex", booktabs=T)
+knitr::kable(summary(cleanCSV[12:16]), "latex", booktabs=T)
+
+str(cleanCSV)
 
 #################Generating Graph Data########################
 
@@ -56,8 +61,6 @@ deaths <- sqldf("SELECT *
 injuries <- sqldf("SELECT *
                 FROM cleanCSV
                   WHERE statistical_murder_flag NOT IN ('true')")
-injuries<-injuries[,-8]
-deaths<-deaths[,-8]
 
 incidentsByLocation <- sqldf("SELECT DISTINCT incident_key, x_coord_cd, y_coord_cd, latitude, longitude
                              FROM cleanCSV")
@@ -65,13 +68,46 @@ incidentsByLocation <- sqldf("SELECT DISTINCT incident_key, x_coord_cd, y_coord_
 
 ########Create the Data Dictionary############################
 varName<-colnames(cleanCSV)
-varType<-c("int", "date", "chr", "chr", "int", "int", "chr", "chr", "chr", "chr", "chr", "chr", "chr", "int", "int", "num", "num")
-varDesc<-c("Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me","Replace Me")
+varType<-c("int", 
+           "date", 
+           "chr", 
+           "chr", 
+           "int", 
+           "chr", 
+           "chr", 
+           "chr", 
+           "chr", 
+           "chr", 
+           "chr", 
+           "chr", 
+           "int", 
+           "int", 
+           "num",
+           "num")
+varDesc<-c("Incidient number, duplicates are victims",
+           "Indicates the date of occurance",
+           "Indicates the time of the incident",
+           "Indicates which borrough the even occured in",
+           "Indicates which police department responded",
+           "Indicates if victim died",
+           "If known, age range of perpetrator",
+           "If known, sex of perpetrator",
+           "If known race of perpetrator ",
+           "Victims Age Range","Victims Sex",
+           "Victims Race",
+           "X grid coordinate for event",
+           "Y grid coordinate for event",
+           "Lattitude of event",
+           "Longitude of event")
 dataDict<-data.frame(varName,varType,varDesc)
 colnames(dataDict)<-c("Variable Name", "Variable Type", "Variable Description")
 
-grid.table(dataDict)
+knitr::kable(dataDict, "latex", booktabs=T)
 
 ########Create the map to plot on#############################
 
+hist_events <- ggplot(data=cleanCSV, aes(x=occur_date))+geom_histogram(aes(color='orange'))
+hist_events
 
+line_deaths <- ggplot(data=cleanCSV, aes(x=occur_date, y=statistical_murder_flag))+geom_line()
+line_deaths
